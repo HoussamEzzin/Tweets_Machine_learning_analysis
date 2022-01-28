dfee4173a83bdcefc7ea0324ab48404c3e3d05e6
Ezzin Houssam <ezzinhoussam@gmail.com>
2022-01-28 16:32:49 +0100

    up to date

--- main.py
+++ main.py
@@ -8,6 +8,31 @@ import nltk
 data_source_url = "https://raw.githubusercontent.com/kolaveridi/kaggle-Twitter-US-Airline-Sentiment-/master/Tweets.csv"
 airline_tweets = pd.read_csv(data_source_url)
 airline_tweets.head()
+features = airline_tweets.iloc[:, 10].values
+labels = airline_tweets.iloc[:, 1].values
+
+processed_features = []
+
+for sentence in range(0, len(features)):
+    # Remove all the special characters
+    processed_feature = re.sub(r'\W', ' ', str(features[sentence]))
+
+    # remove all single characters
+    processed_feature= re.sub(r'\s+[a-zA-Z]\s+', ' ', processed_feature)
+
+    # Remove single characters from the start
+    processed_feature = re.sub(r'\^[a-zA-Z]\s+', ' ', processed_feature) 
+
+    # Substituting multiple spaces with single space
+    processed_feature = re.sub(r'\s+', ' ', processed_feature, flags=re.I)
+
+    # Removing prefixed 'b'
+    processed_feature = re.sub(r'^b\s+', '', processed_feature)
+
+    # Converting to Lowercase
+    processed_feature = processed_feature.lower()
+
+    processed_features.append(processed_feature)
 
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
