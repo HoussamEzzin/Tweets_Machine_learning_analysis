d889af1bfe7f37e95b9ed709e7925ed4ad05786d
Ezzin Houssam <ezzinhoussam@gmail.com>
2022-01-28 16:26:15 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Airline Tweets Sentiment Analysis
+
+- make sure you install numpy pandas nltk before running the code.
---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import pandas as pd 
+import re
+import nltk 
+
+# %matplotlib inline
+
+data_source_url = "https://raw.githubusercontent.com/kolaveridi/kaggle-Twitter-US-Airline-Sentiment-/master/Tweets.csv"
+airline_tweets = pd.read_csv(data_source_url)
+airline_tweets.head()
+
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer (max_features=2500, min_df=7, max_df=0.8, stop_words=stopwords.words('english'))
+processed_features = vectorizer.fit_transform(processed_features).toarray()
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.2, random_state=0)
+from sklearn.ensemble import RandomForestClassifier
+
+text_classifier = RandomForestClassifier(n_estimators=200, random_state=0)
+text_classifier.fit(X_train, y_train)
+
+predictions = text_classifier.predict(X_test)
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+print(confusion_matrix(y_test,predictions))
+print(classification_report(y_test,predictions))
+print(accuracy_score(y_test, predictions))
